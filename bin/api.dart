c1dff11b1204883f3c725b792a452cb124926a55
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-11-15 22:55:58 +0530

    Added POST

--- bin/api.dart
+++ bin/api.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class Api {
+  List data = jsonDecode(
+    File('friends.json').readAsStringSync()
+  )['users'];
   Handler get handler {
     final router = Router();
 
@@ -12,10 +16,26 @@ class Api {
       return Response.ok(
         jsonEncode({
           'success':true,
-          'data':name
+          'data':data
         })
       );
     });
+
+    router.post('/api/add', (Request request) async{
+      final payload = jsonDecode(await request.readAsString());
+      data.add(payload);
+
+      return Response.ok(
+        jsonEncode({
+          'success':true,
+          'data':data
+        }),
+        headers: {
+        'Content-type':'application/json'
+        },
+      );
+    });
+
 
     return router;
   }
